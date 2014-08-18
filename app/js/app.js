5723bcf3a8e127706b7af52fee6fae80357d853a
spirit_only <spiritonly@gmail.com>
2014-08-18 17:48:10 +0800

    move anon control function to controllers.js

--- app/js/app.js
+++ app/js/app.js
@@ -13,38 +13,7 @@ angular.module('angularAdmin', [
             "left-side": {templateUrl: "partials/leftSide.html"},
             "right-side": {
                 templateUrl: "partials/rightSide.html",
-                controller: function($scope){
-                    $scope.salesChart = {
-                        resize: true,
-                        colors: ["#3c8dbc", "#f56954", "#00a65a"],
-                        data: [
-                            {label: "Download Sales", value: 12},
-                            {label: "In-Store Sales", value: 30},
-                            {label: "Mail-Order Sales", value: 20}
-                        ],
-                        hideHover: 'auto'
-                    };
-                    $scope.revenueChart = {
-                        resize: true,
-                        data: [
-                            {y: '2011 Q1', item1: 2666, item2: 2666},
-                            {y: '2011 Q2', item1: 2778, item2: 2294},
-                            {y: '2011 Q3', item1: 4912, item2: 1969},
-                            {y: '2011 Q4', item1: 3767, item2: 3597},
-                            {y: '2012 Q1', item1: 6810, item2: 1914},
-                            {y: '2012 Q2', item1: 5670, item2: 4293},
-                            {y: '2012 Q3', item1: 4820, item2: 3795},
-                            {y: '2012 Q4', item1: 15073, item2: 5967},
-                            {y: '2013 Q1', item1: 10687, item2: 4460},
-                            {y: '2013 Q2', item1: 8432, item2: 5713}
-                        ],
-                        xkey: 'y',
-                        ykeys: ['item1', 'item2'],
-                        labels: ['Item 1', 'Item 2'],
-                        lineColors: ['#a0d0e0', '#3c8dbc'],
-                        hideHover: 'auto'
-                    };
-                }
+                controller: 'DashbaordCtrl'
             }
         }
     });
