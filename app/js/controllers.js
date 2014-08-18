7e198320354129662ec5dd3e8c7d3b18228605cf
spirit_only <spiritonly@gmail.com>
2014-08-18 17:38:20 +0800

    use charanular directives implement draw
    1. add chartanular.js
    2. implement draw sales chart

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,14 @@
 
 /* Controllers */
 
-angular.module('angularAdmin.controllers', [])
-    .controller('Price', ['$scope', function($scope) {
-
-    }]);
+angular.module('angularAdmin', ['ChartAngular'])
+    .controller('SalesChartCtrl', function($scope) {
+        $scope.salesDonut = {
+            data: [
+                {label: "Download Sales", value: 12},
+                {label: "In-Store Sales", value: 30},
+                {label: "Mail-Order Sales", value: 20}
+            ]
+        };
+        $scope.test = "test chart";
+    });
